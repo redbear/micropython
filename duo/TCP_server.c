a9452f0fe1355b79a0bcc27091fda1effa8eb261
ganshun666 <gs451997174@gmail.com>
2016-08-09 15:32:40 +0800

    duo/TCP_server.c : Add TCP_server and TCP_client module

---
+++ duo/TCP_server.c
@@ -0,0 +1,261 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "py/nlr.h"
+#include "py/runtime.h"
+#include "py/binary.h"
+#include "py/mphal.h"
+#include "adc.h"
+#include "pin.h"
+#include "genhdr/pins.h"
+#include "wiring.h"
+#include "TCP_server.h"
+#include "TCP_client.h"
+
+typedef enum {
+  SOCKET_STATE_UNUSED,
+  SOCKET_STATE_USED,
+}socket_state_t;
+
+typedef struct {
+  tcp_server *server;
+  sock_handle_t socket_id;
+  socket_state_t socket_state;
+}tcp_server_t;
+
+typedef struct {
+  tcp_client *client;
+  sock_handle_t socket_id;
+  socket_state_t socket_state;
+}tcp_client_t;
+
+static tcp_server_t servers[MAX_SERVER_SOCKETS];
+static tcp_client_t clients[MAX_CLIENT_SOCKETS];
+
+static uint32_t server_id = 0;
+static uint32_t client_id = 0;
+
+//static uint32_t g_nextSocketId = 0;
+
+void TCP_server_init0(void) {
+  uint8_t i;
+  for(i=0; i<MAX_SERVER_SOCKETS; i++) {
+    servers[i].server = NULL;
+    servers[i].socket_state = SOCKET_STATE_UNUSED;
+  }
+
+  for(i=0; i<MAX_CLIENT_SOCKETS; i++) {
+    clients[i].client = NULL;
+    clients[i].socket_state = SOCKET_STATE_UNUSED;
+  }
+
+  return;
+}
+
+
+STATIC mp_obj_t new_TCP_server(mp_obj_t port) {
+
+	if(server_id < MAX_SERVER_SOCKETS) {
+		servers[server_id].socket_id = server_id;
+		servers[server_id].server = (tcp_server *)(TCPServer_newTCPServer(mp_obj_get_int(port)));
+		servers[server_id++].socket_state = SOCKET_STATE_UNUSED;
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The biggest support 3 servers!"));
+	}
+
+	return mp_const_true;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(new_TCP_server_obj, new_TCP_server);
+
+STATIC mp_obj_t TCP_server_begin(mp_obj_t num_in) {
+	uint32_t server_num = mp_obj_get_int(num_in);
+	if(server_num < MAX_SERVER_SOCKETS ) {
+
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "Parameter range is 0 to 2£¡"));
+	}
+
+	if(servers[server_num].server == NULL) {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The server %d does not exist!", server_num));
+	}
+
+	if(servers[server_num].socket_state == SOCKET_STATE_USED) {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The server %d already used!", server_num));
+	}
+
+	tcp_server *TCP_server = servers[server_num].server;
+
+	if (TCPServer_begin(TCP_server)) {
+		servers[server_num].socket_state = SOCKET_STATE_USED;
+		return mp_const_true;
+	} else {
+		return mp_const_false;
+	}
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(TCP_server_begin_obj, TCP_server_begin);
+
+STATIC mp_obj_t TCP_server_stop(mp_obj_t num_in) {
+	uint32_t server_num = mp_obj_get_int(num_in);
+	if(server_num < MAX_SERVER_SOCKETS ) {
+
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "Parameter range is 0 to 2£¡"));
+	}
+
+	if(servers[server_num].server == NULL) {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The server %d does not exist!", server_num));
+	}
+
+	if(servers[server_num].socket_state == SOCKET_STATE_USED) {
+
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The server %d already unused!", server_num));
+	}
+
+	tcp_server *TCP_server = servers[server_num].server;
+	TCPServer_stop(TCP_server);
+
+	servers[server_num].socket_state = SOCKET_STATE_UNUSED;
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(TCP_server_stop_obj, TCP_server_stop);
+
+STATIC mp_obj_t delete_TCP_server(mp_obj_t num_in) {
+	uint32_t server_num = mp_obj_get_int(num_in);
+	if(server_num < MAX_SERVER_SOCKETS ) {
+
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "Parameter range is 0 to 2£¡"));
+	}
+
+	if(servers[server_num].server == NULL) {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The server %d does not exist!", server_num));
+	}
+
+	TCPServer_deleteTCPServer(servers[server_num].server);
+	servers[server_num].server = NULL;
+	servers[server_num].socket_state = SOCKET_STATE_UNUSED;
+
+	return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(delete_TCP_server_obj, delete_TCP_server);
+
+STATIC mp_obj_t TCP_server_available(mp_obj_t num_in) {
+	uint32_t server_num = mp_obj_get_int(num_in);
+	if(server_num < MAX_SERVER_SOCKETS ) {
+
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "Parameter range is 0 to 2£¡"));
+	}
+
+	if(servers[server_num].server == NULL) {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The server %d does not exist!", server_num));
+	}
+
+	if(servers[server_num].socket_state == SOCKET_STATE_USED) {
+
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The server %d already unused!", server_num));
+	}
+
+	if(client_id < MAX_CLIENT_SOCKETS) {
+		clients[client_id].client = TCPServer_available(servers[server_num].server);
+		clients[client_id].socket_id = client_id;
+		clients[client_id].socket_state == SOCKET_STATE_USED;
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The biggest support 10 clients!"));
+	}
+
+	return MP_OBJ_NEW_SMALL_INT(clients[client_id++].socket_id);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(TCP_server_available_obj, TCP_server_available);
+
+STATIC mp_obj_t TCP_server_write(mp_obj_t num_in, mp_obj_t buf_in, mp_obj_t size_in) {
+	uint32_t server_num = mp_obj_get_int(num_in);
+	if(server_num < MAX_SERVER_SOCKETS ) {
+
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "Parameter range is 0 to 2£¡"));
+	}
+
+	if(servers[server_num].server == NULL) {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The server %d does not exist!", server_num));
+	}
+
+	if(servers[server_num].socket_state == SOCKET_STATE_USED) {
+
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "The server %d already unused!", server_num));
+	}
+
+	tcp_server *TCP_server = servers[server_num].server;
+	int size = 0;
+
+    if(MP_OBJ_IS_STR(buf_in)) {
+    	uint8_t *buf = mp_obj_str_get_str(buf_in);
+    	size = TCPServer_write(TCP_server, buf, (size_t)mp_obj_get_int(size_in));
+
+    } else {
+    	int i = 0;
+    	mp_obj_list_t *buffer = MP_OBJ_TO_PTR(buf_in);
+    	uint8_t *buf = NULL;
+    	buf = (uint8_t *)malloc(buffer->len);
+
+    	for(; i < buffer->len; i++) {
+    		buf[i] = mp_obj_get_int(buffer->items[i]);
+    	}
+    	size = TCPServer_write(TCP_server, buf, (size_t)mp_obj_get_int(size_in));
+
+    	free(buf);
+    }
+
+    return MP_OBJ_NEW_SMALL_INT(size);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(TCP_server_write_obj, TCP_server_write);
+
+STATIC mp_obj_t TCP_server_write_data(mp_obj_t client_id_in, mp_obj_t buf_in, mp_obj_t size_in) {
+    uint32_t client_id = mp_obj_get_int(client_id_in);
+	int size = 0;
+	if(client_id < MAX_CLIENT_SOCKETS ) {
+
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "Parameter range is 0 to 19£¡"));
+	}
+
+	 return TCP_client_write_data(clients[client_id].client, buf_in, size_in);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(TCP_server_write_data_obj, TCP_server_write_data);
+
+STATIC mp_obj_t TCP_server_read_data(mp_obj_t client_id_in, mp_obj_t buf_in, mp_obj_t size_in) {
+    uint32_t client_id = mp_obj_get_int(client_id_in);
+	int size = 0;
+	if(client_id < MAX_CLIENT_SOCKETS ) {
+
+	} else {
+		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_IndentationError, "Parameter range is 0 to 19£¡"));
+	}
+
+	 return TCP_client_read_data(clients[client_id].client, buf_in, size_in);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(TCP_server_read_data_obj, TCP_server_read_data);
+
+STATIC const mp_map_elem_t TCP_server_locals_dict_table[] = {
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_new), (mp_obj_t)&new_TCP_server_obj},
+    { MP_OBJ_NEW_QSTR(MP_QSTR_begin), (mp_obj_t)&TCP_server_begin_obj},
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_stop), (mp_obj_t)&TCP_server_stop_obj},
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_delete), (mp_obj_t)&delete_TCP_server_obj},
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_available), (mp_obj_t)&TCP_server_available_obj},
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_write), (mp_obj_t)&TCP_server_write_obj},
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_write_data), (mp_obj_t)&TCP_server_write_data_obj},
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_read_data), (mp_obj_t)&TCP_server_read_data_obj},
+};
+
+STATIC MP_DEFINE_CONST_DICT(TCP_server_locals_dict, TCP_server_locals_dict_table);
+
+const mp_obj_type_t pyb_TCP_server_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_TCP_server,
+    .locals_dict = (mp_obj_t)&TCP_server_locals_dict,
+};
