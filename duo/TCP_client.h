07406ba5b077f12546f6f5823c734d248e7385ba
ganshun666 <gs451997174@gmail.com>
2016-08-09 15:32:18 +0800

    duo/TCP_client.h : Add TCP_server and TCP_client module

---
+++ duo/TCP_client.h
@@ -0,0 +1,11 @@
+#ifndef DUO_TCP_CLIENT_H_
+#define DUO_TCP_CLIENT_H_
+
+#include "wiring.h"
+
+extern const mp_obj_type_t pyb_TCP_client_type;
+
+mp_obj_t TCP_client_write_data(tcp_client * client_in, mp_obj_t buf_in, mp_obj_t size_in);
+mp_obj_t TCP_client_read_data(tcp_client * client_in, mp_obj_t buf_in, mp_obj_t size_in);
+
+#endif /* DUO_TCP_CLIENT_H_ */
