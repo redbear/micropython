24e60cb516a80a10fcd904221978ed71ca6fd76b
ganshun666 <gs451997174@gmail.com>
2016-08-18 11:12:24 +0800

    duo/TCP_client.h : Optimize the code.

--- duo/TCP_client.h
+++ duo/TCP_client.h
@@ -5,7 +5,7 @@
 
 extern const mp_obj_type_t pyb_TCP_client_type;
 
-mp_obj_t TCP_client_write_data(tcp_client * client_in, mp_obj_t buf_in, mp_obj_t size_in);
+mp_obj_t TCP_client_write_data(tcp_client * client_in, mp_obj_t buf_in);
 mp_obj_t TCP_client_read_data(tcp_client * client_in, mp_obj_t buf_in, mp_obj_t size_in);
 
 #endif /* DUO_TCP_CLIENT_H_ */
