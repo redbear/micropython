14c2b6dcbe839f37bba4a91fee1e4be26e21b622
ganshun666 <gs451997174@gmail.com>
2016-08-18 09:07:05 +0800

    duo/TCP_server.h : Optimize the code.

--- duo/TCP_server.h
+++ duo/TCP_server.h
@@ -4,6 +4,12 @@
 #define MAX_SERVER_SOCKETS  3
 #define MAX_CLIENT_SOCKETS  20
 
+typedef enum {
+    TCP_SERVER_0 = 0,
+    TCP_SERVER_1 = 1,
+	TCP_SERVER_2 = 2,
+} pyb_TCP_server_t;
+
 void TCP_server_init0(void);
 
 extern const mp_obj_type_t pyb_TCP_server_type;
